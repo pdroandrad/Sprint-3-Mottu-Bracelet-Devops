ee6f52d60b97f7f897c502069fd45f099a1f2d6a
Pedro Andrade <161619263+pdroandrad@users.noreply.github.com>
2025-09-22 17:54:42 -0300

    Update AppDbContext.cs

--- MottuBracelet/Data/AppDbContext.cs
+++ MottuBracelet/Data/AppDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MottuBracelet.Model;
 
 namespace MottuBracelet.Data
@@ -9,24 +9,27 @@ namespace MottuBracelet.Data
         {
         }
 
-        public DbSet<Patio> Patio { get; set; }
-        public DbSet<Dispositivo> Dispositivo { get; set; }
-        public DbSet<Moto> Moto { get; set; }
-        public DbSet<HistoricoPatio> HistoricoPatio { get; set; }
+        public DbSet<Patio> Patios { get; set; }
+        public DbSet<Dispositivo> Dispositivos { get; set; }
+        public DbSet<Moto> Motos { get; set; }
+        public DbSet<HistoricoPatio> HistoricoPatios { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Mapear nomes das tabelas no SQL Server
             modelBuilder.Entity<Moto>().ToTable("MOTO_NET");
             modelBuilder.Entity<Patio>().ToTable("PATIO_NET");
             modelBuilder.Entity<Dispositivo>().ToTable("DISPOSITIVO_NET");
             modelBuilder.Entity<HistoricoPatio>().ToTable("HISTORICOPATIO_NET");
 
+            // Relação 1:1 entre Moto e Dispositivo
             modelBuilder.Entity<Moto>()
                 .HasOne(m => m.Dispositivo)
                 .WithOne(d => d.Moto)
                 .HasForeignKey<Dispositivo>(d => d.MotoId)
                 .OnDelete(DeleteBehavior.SetNull);
 
+            // Endereço é um tipo próprio dentro de Patio
             modelBuilder.Entity<Patio>()
                 .OwnsOne(p => p.Endereco);
         }
